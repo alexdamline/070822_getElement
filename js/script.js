8bf297f58ddfef2381d89de9b3684d62d76e54a4
Alex <alexdamline@gmail.com>
2022-08-07 09:16:17 +0300

    Beautify

--- js/script.js
+++ js/script.js
@@ -1,40 +1,20 @@
 "use strict";
 
-//let counterLeftValue =
-//document.getElementById('counterLeft');
 
-//++counterLeftValue;
-
-
-//console.log('counterLeftValue');
-// counterLeftValue.style.color = "red";
-
-
-//const value = 8;
 let leftV = document.getElementsByTagName('h2')[0];
-//h2.innerHTML = value + 1;
 
 let a = 0;
-//if (1 > 0) {
-//  ++a;
-//}
 
-function valueLeft(){
-  leftV.innerHTML = ++a;
+function valueLeft() {
+    leftV.innerHTML = ++a;
 }
 
-let rightV= document.getElementsByTagName('h2')[1];
-//h2.innerHTML = value + 1;
+let rightV = document.getElementsByTagName('h2')[1];
 
 let b = 0;
-//if (1 > 0) {
-//  ++a;
-//}
 
-function valueRight(){
-  rightV.innerHTML = ++b;
+function valueRight() {
+    rightV.innerHTML = ++b;
 }
 
 console.log(a);
-
-//console.log(typeof(h2.innerHTML));
