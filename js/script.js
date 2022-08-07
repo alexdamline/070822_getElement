f5a5375d7614ed4bb4ff4dac6f7a9b5cd63a85d2
Alex <alexdamline@gmail.com>
2022-08-07 20:47:19 +0300

    1. Clean files | 2. Add comments

--- js/script.js
+++ js/script.js
@@ -1,20 +1,21 @@
 "use strict";
 
-
+//Take value of first h2 element
 let leftV = document.getElementsByTagName('h2')[0];
 
 let a = 0;
 
+//After click "buttonGood" increase first h2 element value on 1 
 function valueLeft() {
     leftV.innerHTML = ++a;
 }
 
+//Take value of second h2 element
 let rightV = document.getElementsByTagName('h2')[1];
 
 let b = 0;
 
+//After click "buttonGood" increase second h2 element value on 1 
 function valueRight() {
     rightV.innerHTML = ++b;
 }
-
-console.log(a);
